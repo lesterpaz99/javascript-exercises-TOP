89a8d039e85917f29f7deadb99fd4092fe5ef723
Obed Paz <lesterpaz99@gmail.com>
2024-09-23 22:22:32 -0600

    pass the exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+	if (num === 0) return '';
+	if (num < 0) return 'ERROR';
+	if (str === '') return '';
 
+	let finalStr = str;
+
+	for (let i = 1; i < num; i++) {
+		finalStr += str;
+	}
+
+	return finalStr;
 };
 
 // Do not edit below this line
