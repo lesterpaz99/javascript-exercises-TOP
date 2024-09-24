3a679b82c2b491681e3a4cb3413846228637eb08
Obed Paz <lesterpaz99@gmail.com>
2024-09-23 22:25:32 -0600

    pass the reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function (strValue) {
+	if (strValue === '') return '';
 
+	const splittedStr = strValue.split('');
+	return splittedStr.reverse().join('');
 };
 
 // Do not edit below this line
