2d8ed6f7209506b58016f6103dad293d934f19a5
Obed Paz <lesterpaz99@gmail.com>
2024-09-26 22:29:16 -0600

    pass challenge 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...itemsToDelete) {
+	if (itemsToDelete.length === 1) {
+		const finalArr = arr.filter((item) => {
+			return item !== itemsToDelete[0];
+		});
+		return finalArr;
+	}
 
+	const finalArr = [...arr];
+	itemsToDelete.forEach((itemToDelete) => {
+		const itemIndex = finalArr.findIndex((item) => item === itemToDelete);
+
+		if (itemIndex !== -1) {
+			finalArr.splice(itemIndex, 1);
+		}
+	});
+
+	return finalArr;
+};
 // Do not edit below this line
 module.exports = removeFromArray;
