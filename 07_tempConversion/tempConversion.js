403a0332cfd6548cfd20c5d28be2ee8ebc8f73e7
Obed Paz <lesterpaz99@gmail.com>
2024-09-27 23:59:25 -0600

    pass challenge tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (gradesInF) {
+	const gradesConverted = (gradesInF - 32) * (5 / 9);
+	const roundedResult = (Math.round(gradesConverted * 10) / 10).toFixed(1);
+	return Number(roundedResult);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (gradesInC) {
+	const gradesConverted = gradesInC * (9 / 5) + 32;
+	const roundedResult = (Math.round(gradesConverted * 10) / 10).toFixed(1);
+	return Number(roundedResult);
 };
-
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
