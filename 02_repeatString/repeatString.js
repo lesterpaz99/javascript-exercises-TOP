c868a316a817ffed7aaa00a4a805e9a69224a13f
Obed Paz <lesterpaz99@gmail.com>
2024-09-27 20:54:18 -0600

    refactor repeatString challenge solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,8 @@
 const repeatString = function (str, num) {
-	if (num === 0) return '';
+	if (num === 0 || str === '') return '';
 	if (num < 0) return 'ERROR';
-	if (str === '') return '';
 
 	let finalStr = str;
-
 	for (let i = 1; i < num; i++) {
 		finalStr += str;
 	}
