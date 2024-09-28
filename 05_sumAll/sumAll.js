e8421f0470cbd1c9a24f1a410e8626e45f69e2a0
Obed Paz <lesterpaz99@gmail.com>
2024-09-27 21:48:55 -0600

    pass the sumAll chalenge

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	const numberArr = [num1, num2];
 
+	const isInvalidNumber = (x) =>
+		Math.sign(x) === -1 || !Number.isInteger(x) || isNaN(x);
+
+	if (numberArr.some(isInvalidNumber)) {
+		return 'ERROR';
+	}
+	const [value1, value2] = numberArr.sort((a, b) => a - b);
+
+	let numbers = [];
+	for (let i = value1; i <= value2; i++) {
+		numbers.push(i);
+	}
+
+	return numbers.reduce((prev, current) => prev + current);
 };
 
 // Do not edit below this line
