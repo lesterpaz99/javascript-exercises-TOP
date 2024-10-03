68932897b41c6c50f7912baec27dde5a44b22ba4
Obed Paz <lesterpaz99@gmail.com>
2024-10-02 20:23:45 -0600

    pass the fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+	const number = Number(num);
+	if (number < 0) return 'OOPS';
+	if (number === 0) return 0;
+	if (number === 1 || number === 2) return 1;
 
+	let prev = 1,
+		current = 1;
+
+	for (let i = 3; i <= number; i++) {
+		const next = prev + current;
+		prev = current;
+		current = next;
+	}
+
+	return current;
 };
 
 // Do not edit below this line
