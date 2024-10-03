a6ba5d61a364787a60687c25a87a7e02aef47d5c
Obed Paz <lesterpaz99@gmail.com>
2024-10-02 19:12:12 -0600

    pass the palindrome challenge

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+	let regex = /[\W]/g;
+	const formattedStr = str.toLowerCase().replaceAll(regex, '');
+	const arr = formattedStr.split('');
+	const reversedArr = [...arr].reverse();
 
+	return arr.every((item, i) => item === reversedArr[i]);
 };
 
 // Do not edit below this line
