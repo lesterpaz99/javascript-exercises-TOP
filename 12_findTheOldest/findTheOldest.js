1fbc430f72591cef3794b438b9d1087bcc9dc325
Obed Paz <lesterpaz99@gmail.com>
2024-10-02 23:11:54 -0600

    pass last challenge: findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArr) {
+	const ages = peopleArr.map((person) => ({
+		name: person.name,
+		age: person.yearOfDeath
+			? person.yearOfDeath - person.yearOfBirth
+			: new Date().getFullYear() - person.yearOfBirth,
+	}));
 
+	const sortedAges = ages.sort((a, b) => a.age - b.age);
+	return sortedAges.at(-1);
 };
 
 // Do not edit below this line
